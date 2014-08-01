e002a7de1fc347b707d3d0057cc7419934da4537
Olga Ivanova <oivanova@ronasit.com>
2014-08-01 19:55:27 +0700

    Brand fix

--- app/code/community/Lipscore/RatingsReviews/Helper/Product.php
+++ app/code/community/Lipscore/RatingsReviews/Helper/Product.php
@@ -15,13 +15,24 @@ class Lipscore_RatingsReviews_Helper_Product extends Lipscore_RatingsReviews_Hel
         $idType    = $this->_lipscoreConfig->get('type', 'identifier');
         $idAttr    = $this->_lipscoreConfig->get('attr', 'identifier');
         $brandAttr = $this->_lipscoreConfig->get('attr', 'brand');
-        
+
         return array(
             'name'   => $product->getName(),
-            'brand'  => $brandAttr ? $product->getAttributeText($brandAttr) : null,
+            'brand'  => $brandAttr ? $this->_getAttributeValue($product, $brandAttr) : null,
             'idType' => $idType,
-            'id'     => $idAttr ? $product->getData($idAttr) : null,
+            'id'     => $idAttr ? $this->_getAttributeValue($product, $idAttr) : null,
             'url'    => $product->getProductUrl()
         );
+    }
+    
+    protected function _getAttributeValue(Mage_Catalog_Model_Product $product, $attrCode)
+    {
+        $attr = $product->getResource()->getAttribute($attrCode);
+        
+        if ('select' == $attr->getFrontendInput()) {
+            return $attr->getSource()->getOptionText($product->getData($attrCode));
+        } else {
+            return $product->getData($attrCode);
+        }
     }
 }
