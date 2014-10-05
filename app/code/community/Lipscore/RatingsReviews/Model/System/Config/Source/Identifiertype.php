04c01d028f64900d8a21d5447f2527d46894ebfb
Olga Ivanova <oivanova@ronasit.com>
2014-10-05 09:41:01 +0700

    Fix of case sensitive file names.

---
+++ app/code/community/Lipscore/RatingsReviews/Model/System/Config/Source/Identifiertype.php
@@ -0,0 +1,14 @@
+<?php
+ 
+class Lipscore_RatingsReviews_Model_System_Config_Source_Identifiertype
+{
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => 'ean', 'label' => Mage::helper('adminhtml')->__('EAN')),
+            array('value' => 'mpn', 'label' => Mage::helper('adminhtml')->__('MPN')),
+            array('value' => 'sku', 'label' => Mage::helper('adminhtml')->__('SKU')),
+            array('value' => 'upc', 'label' => Mage::helper('adminhtml')->__('UPC')),
+        );
+    }
+}
