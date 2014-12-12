5cd9f3628a2ddb3095f03f00d71e909a83fff40c
Olga Ivanova <snorra.ivanova@gmail.com>
2014-12-12 16:38:37 +0600

    New product identifiers

--- app/code/community/Lipscore/RatingsReviews/Model/System/Config/Source/Identifiertype.php
+++ app/code/community/Lipscore/RatingsReviews/Model/System/Config/Source/Identifiertype.php
@@ -5,10 +5,12 @@ class Lipscore_RatingsReviews_Model_System_Config_Source_Identifiertype
     public function toOptionArray()
     {
         return array(
-            array('value' => 'ean', 'label' => Mage::helper('adminhtml')->__('EAN')),
-            array('value' => 'mpn', 'label' => Mage::helper('adminhtml')->__('MPN')),
-            array('value' => 'sku', 'label' => Mage::helper('adminhtml')->__('SKU')),
-            array('value' => 'upc', 'label' => Mage::helper('adminhtml')->__('UPC')),
+            array('value' => 'ean',  'label' => Mage::helper('adminhtml')->__('EAN')),
+            array('value' => 'isbn', 'label' => Mage::helper('adminhtml')->__('ISBN')),
+            array('value' => 'mbid', 'label' => Mage::helper('adminhtml')->__('MBID')),
+            array('value' => 'mpn',  'label' => Mage::helper('adminhtml')->__('MPN')),
+            array('value' => 'sku',  'label' => Mage::helper('adminhtml')->__('SKU')),
+            array('value' => 'upc',  'label' => Mage::helper('adminhtml')->__('UPC')),
         );
     }
 }
