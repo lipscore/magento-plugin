04f89954d46a1dc6478e4423dec352aa3f54e40b
Olga Ivanova <snorra.ivanova@gmail.com>
2015-11-05 21:06:12 +0600

    Fix of events of 'reviews(count)' link

--- skin/frontend/base/default/js/lipscore.js
+++ skin/frontend/base/default/js/lipscore.js
@@ -1,18 +1,24 @@
-document.observe("dom:loaded", function() {
+function magentoLipscoreInit() {
+    tabSelector = '.lipscore-reviews-tab';
+    if ($$(tabSelector).length > 0) {
+        // show review count
+        lipscore.on('review-count-set', function(data) {
+            if (data.value > 0) {
+                $$('.lipscore-reviews-tab-count').invoke('show');
+            } 
+        });
+        
+        // open reviews tab if reviews link clicked            
+        lipscore.on('review-count-link-clicked', function(data) {
+            $$(tabSelector)[0].up().click();
+        });
+    }
+}
+
+document.observe('dom:loaded', function() {
     if (typeof lipscore !== 'undefined') {
-        tabSelector = '.lipscore-reviews-tab';
-        if ($$(tabSelector).length > 0) {
-            // show review count
-            lipscore.on('review-count-set', function(data) {
-                if (data.value > 0) {
-                    $$('.lipscore-reviews-tab-count').invoke('show');
-                } 
-            });
-            
-            // open reviews tab if reviews link clicked            
-            lipscore.on('review-count-link-clicked', function(data) {
-                $$(tabSelector)[0].up().click();
-            });
-        }
-    }   
+        magentoLipscoreInit();
+    } else {
+        document.observe('lipscore-created', magentoLipscoreInit);
+    }
 });
