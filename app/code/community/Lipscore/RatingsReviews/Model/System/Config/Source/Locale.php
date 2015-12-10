d7c002d6c4cb243b969f26ebbf89fb437f7f6c79
Olga Ivanova <snorra.ivanova@gmail.com>
2015-12-10 16:19:27 +0600

    add Japanese locale

--- app/code/community/Lipscore/RatingsReviews/Model/System/Config/Source/Locale.php
+++ app/code/community/Lipscore/RatingsReviews/Model/System/Config/Source/Locale.php
@@ -11,6 +11,7 @@ class Lipscore_RatingsReviews_Model_System_Config_Source_Locale
             array('value' => 'nl',   'label' => Mage::helper('adminhtml')->__('Dutch')),
             array('value' => 'en',   'label' => Mage::helper('adminhtml')->__('English')),
             array('value' => 'it',   'label' => Mage::helper('adminhtml')->__('Italian')),
+            array('value' => 'ja',   'label' => Mage::helper('adminhtml')->__('Japanese')),
             array('value' => 'no',   'label' => Mage::helper('adminhtml')->__('Norwegian')),
             array('value' => 'br',   'label' => Mage::helper('adminhtml')->__('Portuguese (Brazil)')),
             array('value' => 'ru',   'label' => Mage::helper('adminhtml')->__('Russian')),
