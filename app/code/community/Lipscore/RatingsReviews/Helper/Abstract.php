68aa1a29f6cd42a506f6c00716c7a737b6b08f9b
Olga Ivanova <snorra.ivanova@gmail.com>
2016-02-20 13:39:45 +0600

    Saving files before refreshing line endings

--- app/code/community/Lipscore/RatingsReviews/Helper/Abstract.php
+++ app/code/community/Lipscore/RatingsReviews/Helper/Abstract.php
@@ -6,12 +6,20 @@ abstract class Lipscore_RatingsReviews_Helper_Abstract extends Mage_Core_Helper_
      * @var Lipscore_RatingsReviews_Model_Config
      */
     protected $_lipscoreConfig;
-    
+
+    public function __call($method, $arguments) {
+        try {
+            call_user_func_array(array($this, $method), $arguments);
+        } catch (Exception $e) {
+            Lipscore_RatingsReviews_Logger::logException($e);
+        }
+    }
+
     public function __construct()
     {
         $this->_lipscoreConfig = Mage::getModel('lipscore_ratingsreviews/config');
     }
-    
+
     public function setLipscoreConfig($lipscoreConfig)
     {
         $this->_lipscoreConfig = $lipscoreConfig;
